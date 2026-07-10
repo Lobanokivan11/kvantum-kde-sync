4c096d81d36ec69143c95aaf7add8ff287755a47
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2026-07-10 20:54:28 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
+#!/bin/bash
+echo "Installing Script..."
 mkdir -p ~/.local/bin
 cp kvantum-theme-watcher.sh ~/.local/bin/kvantum-theme-watcher.sh 
 chmod +x ~/.local/bin/kvantum-theme-watcher.sh
+echo "Installing Systemd Unit..."
 mkdir -p ~/.config/systemd/user
 cp kvantum-watcher.service ~/.config/systemd/user/kvantum-watcher.service
+echo "Enabling Systemd Unit..."
 systemctl --user daemon-reload                                                
 systemctl --user enable --now kvantum-watcher.service
+echo "Install is ended succesfully..."
