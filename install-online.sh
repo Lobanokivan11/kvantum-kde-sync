acb1a0948f1349a79a1fe22b7593b6b90811d59c
Lobanokivan11 <lobanokivan11@gmail.com>
2026-07-11 11:59:05 +0300

    add

--- install-online.sh
+++ install-online.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Installing Script..."
 mkdir -p ~/.local/bin
-wget -O ~/.local/bin/kvantum-theme-watcher.sh https://raw.githubusercontent.com/Lobanokivan11/kvantum-kde-sync/refs/heads/main/kvantum-theme-watcher.sh
-chmod +x ~/.local/bin/kvantum-theme-watcher.sh
+wget -O ~/.local/bin/kvantum-theme-watcher https://raw.githubusercontent.com/Lobanokivan11/kvantum-kde-sync/refs/heads/main/kvantum-theme-watcher
+chmod +x ~/.local/bin/kvantum-theme-watcher
 echo "Installing Systemd Unit..."
 mkdir -p ~/.config/systemd/user
 wget -O ~/.config/systemd/user/kvantum-watcher.service https://raw.githubusercontent.com/Lobanokivan11/kvantum-kde-sync/refs/heads/main/kvantum-watcher.service
